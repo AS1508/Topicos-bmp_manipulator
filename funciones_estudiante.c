82a683c7e43777ebb3ed1d96b9a0a5b7b013783f
Sebastián Martínez <70720135+AS1508@users.noreply.github.com>
2024-04-21 20:56:21 -0300

    Update funciones_estudiante.c

--- funciones_estudiante.c
+++ funciones_estudiante.c
@@ -23,35 +23,10 @@
 
 */
 
-#include "constantes.h"
-
-int solucion(int argc, char* argv[])
+void solucion()
 {
 /*
     Aquí deben hacer el código que solucione lo solicitado.
     Todas las funciones utilizadas deben estar declaradas en este archivo, y en su respectivo .h
-
-
-    while(arg)
-        if(!bmp())
-            char[][] = argv*
-        
-        menu(char[][], foto)
-
-    menu(
-        switch
-
-    )
-
-    fun
-        print iniciando
-        proceso
-        print finalizando
-        system(pause)
-
-
-        t_pixeles[] 1235    5321
-        11 23 4f
 */
-    return TODO_OK;
 }
