3e1eaef092cd8ed4a0b5083ca214a9b31119f399
Sebastián Martínez <70720135+AS1508@users.noreply.github.com>
2024-05-06 00:35:14 -0300

    update
    (quiero morir)

--- funciones_estudiante.c
+++ funciones_estudiante.c
@@ -2,14 +2,14 @@
     Integrantes del grupo. En caso de ser un grupo de dos integrantes, no completar el último campo.
     Si alguno de los integrantes del grupo dejara la materia, completar de todos modos sus datos, aclarando que no entrega.
     -----------------
-    Apellido: Martinez Rodio
-    Nombre: Alan Sebastian
-    DNI: 45206315
+    Apellido:
+    Nombre:
+    DNI:
     Entrega:
     -----------------
-    Apellido: Risso
-    Nombre:Santiago Nicolas
-    DNI: 44552484
+    Apellido:
+    Nombre:
+    DNI:
     Entrega:
     -----------------
     (Sólo para grupos de tres integrantes)
@@ -21,47 +21,51 @@
 
     Comentarios (opcionales) que deseen hacer al docente sobre el TP:
 
-    **
-        Aquí deben hacer el código que solucione lo solicitado.
-        Todas las funciones utilizadas deben estar declaradas en este archivo, y en su respectivo .h
-    **
-
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "constantes.h"
+#include "estructuras.h"
 #include "funciones_estudiante.h"
 
-
-void solucion(int argc, char* argv[])
+int solucion(int argc, char* argv[])
 {
-    int i=0, cantidadArgumentos = 0;
-    char argumentos[12][TAM], nombreArchivo[TAM];
+   int i=0, cantidadArgumentos = 0;
+    char argumentos[12][TAM], nombreArchivo[TAM] = {0}, buffer[TAM] = {0};
     t_imagen imagen;
+
 
     while(i<argc)
     {
-        if((!buscar(argv, ARGS, C_ARG)) && (!buscar(argv, IMG, TAM)))
-                printf("Argumento/imagen invalido\n");
+        printf("%s: ", argv[i]);
+        strcpy(buffer, argv[i]);
 
-        if(buscar(argv, ARGS, C_ARG) == 1)
+        if((buscar(buffer, ARGS)) == 0 && (buscar(buffer, IMG)) == 0)
+            printf("Argumento/imagen invalido\n");
+
+        if(buscar(buffer, ARGS) == 1)
         {
+            printf("paso por arg\n");
             cantidadArgumentos++;
             strcpy(argumentos[i], argv[i]);
-            i++;
         }
-        if(buscar(argv, IMG, TAM) == 1)
+        if(buscar(buffer, IMG) == 2)
         {
             strcpy(nombreArchivo, argv[i]);
-            cargaArchivo(&imagen, nombreArchivo);
+            //cargaArchivo(&imagen, nombreArchivo);
             menu(&imagen, argumentos, cantidadArgumentos, nombreArchivo);
             cantidadArgumentos = 0;
+            printf("Paso por img\n");
         }
 
         i++;
     }
 
+    return TODO_OK;
 }
+
 
 /*######## Menu ########*/
 
@@ -76,6 +80,8 @@ void menu(t_imagen* imagen, char args[][TAM], int cantidad, char nombre[])
                 printf("Error");
         if(strcmp(args[i], funciones[i]) == 0)
             dump(imagen, nombre);
+        if(strcmp(args[i], funciones[i]) == 0)
+            escalaGrises(funciones, 3, nombre);
 
         args++;
         i++;
@@ -86,27 +92,94 @@ void menu(t_imagen* imagen, char args[][TAM], int cantidad, char nombre[])
 /*######## Argumentos ########*/
 
 
+void escalaGrises(char arg[][TAM], int posicion, char arch[])
+{
+    int i = 0, cont = 0;
+
+    t_metadata metadataOriginal;
+    t_pixel pixelOriginal;
+    t_pixel nuevoPixel;
+
+    FILE* archivo = fopen(arch, "rb");
+    if(archivo == NULL)
+    {
+        printf("No se pudo abrir el archivo\n");
+        exit(1);
+    }
+
+    strcat(arch, arg[posicion]);
+
+    FILE* archivoNuevo = fopen(arch, "wb");
+    if(archivoNuevo == NULL)
+    {
+        printf("No se pudo abrir el archivo\n");
+        exit(1);
+    }
+
+    printf("Creando Archivo \"%s\"\n", arg);
+
+    metadataOriginal = copyData(archivo);
+
+    fwrite(&metadataOriginal, sizeof(t_metadata), 1, archivoNuevo);
+    while(!feof(archivo))
+        fwrite(&metadataOriginal, sizeof(t_metadata), 1, archivoNuevo);
+
+    fread(&pixelOriginal, sizeof(t_pixel), 1, archivo);
+    while(!feof(archivo))
+    {
+        printf("Paso");
+        cont++;
+        fread(&pixelOriginal, sizeof(t_pixel), 1,archivo);
+    }
+    printf("%d\n", cont);
+    while(!feof(archivo))
+    {
+        printf("Paso");
+        nuevoPixel.pixel[0] = (pixelOriginal.pixel[0] + pixelOriginal.pixel[1] + pixelOriginal.pixel[2])/3;
+        nuevoPixel.pixel[1] = (pixelOriginal.pixel[0] + pixelOriginal.pixel[1] + pixelOriginal.pixel[2])/3;
+        nuevoPixel.pixel[2] = (pixelOriginal.pixel[0] + pixelOriginal.pixel[1] + pixelOriginal.pixel[2])/3;
+        nuevoPixel.profundidad = pixelOriginal.profundidad;
+        fwrite(&nuevoPixel, sizeof(nuevoPixel), 1, archivoNuevo);
+        fread(&pixelOriginal, sizeof(pixelOriginal), 1, archivo);
+    }
+
+    printf("Archivo creado \"%s\"con exito", grises);
+    fclose(archivo);
+    fclose(archivoNuevo);
+
+}
 
 /*######## Funcionalidades ########*/
 
-void dump(t_imagen* imagen, char nombre[])
+//void dump(t_imagen* imagen, char nombre[])
 {
     //FILE file;
     //file = fopen( nombre ,"rb");
     printf("Paso por dump");
 }
 
-int buscar(char *argv[], char comando, int cantidad)
+int buscar(char arg[], char comando)
 {
-    printf("Pase por Buscar ");
+    int i = 0, cont = strlen(arg);
 
-    return 1;
+    if(arg[i] == comando && arg[i+1] == comando)
+    {
+        return 1;
+    }
+    if(arg[cont-4] == comando && arg[cont-3] == 'b' && arg[cont-2] == 'm' && arg[cont-1] == 'p')
+    {
+        return 2;
+    }
+    return 0;
 }
 
 void cargaArchivo(t_imagen* imagen, char *nombreArchivo)
 {
     FILE *file = fopen(nombreArchivo, "rb");
-    if(file == NULL){ exit(1); }
+    if(file == NULL)
+    {
+        exit(1);
+    }
 
     while(!feof(file))
         fwrite(&imagen, sizeof(t_imagen), 1, file);
@@ -114,4 +187,12 @@ void cargaArchivo(t_imagen* imagen, char *nombreArchivo)
     fclose(file);
 
 }
-//void copiaArchivo(t_pixel* pixel, t_metadata* datos, t_imagen* imagen) {    /*continue*/    }
+
+t_metadata copyData(FILE* archivo)
+{
+    t_metadata metadata;
+    fread(&metadata, sizeof(t_metadata), 1, archivo);
+    while(!feof(archivo))
+        fread(&metadata, sizeof(t_metadata), 1, archivo);
+    return metadata;
+}
--- funciones_estudiante.h
+++ funciones_estudiante.h
@@ -1,6 +1,7 @@
 #ifndef FUNCIONES_ESTUDIANTE_H_INCLUDED
 #define FUNCIONES_ESTUDIANTE_H_INCLUDED
 #include "estructuras.h"
+#include "constantes.h"
 #define C_ARG 2
 #define TAM 30
 #define ARGS '-'
@@ -8,22 +9,25 @@
 
 typedef struct
 {
-    unsigned char tipoDato[2];
     t_metadata datos;
-    t_pixel imagen;
+    t_pixel pixeles;
 } t_imagen;
 
-void solucion(int argc, char* argv[]);
-void menu(t_imagen* imagen, char args[][TAM], int cantidad, char nombre[]);
-void cargaArchivo(t_imagen *imagen, char *nombreArchivo);
-void copiaArchivo(t_pixel *pixel, t_metadata *datos, t_imagen *imagen);
-void ordenarReflejo(t_pixel *pixel);
-void rotarIzquierda(t_pixel *pixel);
-void rotarDerecha(t_pixel *pixel);
-int buscar(char *argv[], char comando, int cantidad);
+int solucion(int argc, char* argv[]);
+int buscar(char arg[], char comando);
 
-void dump(t_imagen *imagen, char nombre[]);
+void menu(t_imagen imagen, char args[][TAM], int cantidad, char nombre[])
+
+//void cargaArchivo(t_imagen imagen, char *nombreArchivo);
+t_metadata copyData(FILE* archivo);
+
+void escalaGrises(char arg[][TAM], int posicion, char arch[]);
+void ordenarReflejo(t_pixel pixel);
+void rotarIzquierda(t_pixel pixel);
+void rotarDerecha(t_pixel pixel);
+void dump(t_imagen imagen, char nombre[]);
 //Continue
+
 
 
 #endif // FUNCIONES_ESTUDIANTE_H_INCLUDED
